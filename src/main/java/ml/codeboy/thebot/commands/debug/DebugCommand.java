b4d67b4a126064cd440c027b32b8bdc3c14cd216
NilEis <52820950+NilEis@users.noreply.github.com>
2022-07-01 17:31:05 +0200

    Resolves issue: https://github.com/the-codeboy/mensabot/pull/5#discussion_r911378106

--- src/main/java/ml/codeboy/thebot/commands/debug/DebugCommand.java
+++ src/main/java/ml/codeboy/thebot/commands/debug/DebugCommand.java
@@ -1,5 +1,6 @@
 package ml.codeboy.thebot.commands.debug;
 
+import ml.codeboy.thebot.Config;
 import ml.codeboy.thebot.commands.Command;
 import ml.codeboy.thebot.events.CommandEvent;
 
@@ -11,12 +12,7 @@ public abstract class DebugCommand extends Command {
 
     @Override
     public void execute(CommandEvent event) {
-        if (
-                !event.getUser().getId().equals("412330776886247424")
-                        && !event.getUser().getId().equals("902979780394221648")
-                        && !event.getUser().getId().equals("358247499531681803")
-        ) {
-            event.replyError("Not a valid user");
+        if (!Config.getInstance().isDebugAccount(event.getUser().getId())) {
             return;
         }
         super.execute(event);
