fd9db77ee21ad50883fa4eac96b8fa694e880e7e
the-codeboy <71213855+the-codeboy@users.noreply.github.com>
2023-03-08 17:15:08 +0100

    add slash command for latex

--- src/main/java/ml/codeboy/thebot/commands/LatexCommand.java
+++ src/main/java/ml/codeboy/thebot/commands/LatexCommand.java
@@ -4,6 +4,8 @@ import ml.codeboy.thebot.events.CommandEvent;
 import ml.codeboy.thebot.events.MessageCommandEvent;
 import ml.codeboy.thebot.util.Replyable;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 import java.awt.*;
 import java.net.URLEncoder;
@@ -11,6 +13,11 @@ import java.net.URLEncoder;
 public class LatexCommand extends Command {
     public LatexCommand() {
         super("latex", "turns latex into an image", "tex", "");
+    }
+
+    @Override
+    public SlashCommandData getCommandData() {
+        return super.getCommandData().addOption(OptionType.STRING,"latex","the latex you want to render",true);
     }
 
     public static void respondLatex(String latex, Replyable replyable) {
@@ -26,8 +33,7 @@ public class LatexCommand extends Command {
         if (event.isMessageEvent()) {
             message = ((MessageCommandEvent) event).getContent();
         } else {
-            //no slash command support for now
-            return;
+            message=event.getSlashCommandEvent().getOption("latex").getAsString();
         }
         respondLatex(message, event);
     }
