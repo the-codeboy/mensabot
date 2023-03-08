2fb160f0c04b0e2e6940cfc3a1438c771b1e79d0
the-codeboy <71213855+the-codeboy@users.noreply.github.com>
2023-03-08 17:12:27 +0100

    add error when latex too long

--- src/main/java/ml/codeboy/thebot/commands/LatexCommand.java
+++ src/main/java/ml/codeboy/thebot/commands/LatexCommand.java
@@ -3,27 +3,32 @@ package ml.codeboy.thebot.commands;
 import ml.codeboy.thebot.events.CommandEvent;
 import ml.codeboy.thebot.events.MessageCommandEvent;
 import ml.codeboy.thebot.util.Replyable;
+import net.dv8tion.jda.api.EmbedBuilder;
 
+import java.awt.*;
 import java.net.URLEncoder;
 
-public class LatexCommand extends Command{
+public class LatexCommand extends Command {
     public LatexCommand() {
-        super("latex", "turns latex into an image", "tex","");
+        super("latex", "turns latex into an image", "tex", "");
+    }
+
+    public static void respondLatex(String latex, Replyable replyable) {
+        if (latex.length() > 200) {
+            replyable.reply(new EmbedBuilder().setTitle("This is too much text").setDescription("the maximum is 200 characters :(").setColor(Color.RED).build());
+        } else
+            replyable.reply("https://chart.apis.google.com/chart?cht=tx&chl=" + URLEncoder.encode(latex), true);
     }
 
     @Override
     public void run(CommandEvent event) {
         String message;
-        if(event.isMessageEvent()){
-            message=((MessageCommandEvent)event).getContent();
-        }else {
+        if (event.isMessageEvent()) {
+            message = ((MessageCommandEvent) event).getContent();
+        } else {
             //no slash command support for now
             return;
         }
-        respondLatex(message,event);
-    }
-
-    public static void respondLatex(String latex, Replyable replyable){
-        replyable.reply("https://chart.apis.google.com/chart?cht=tx&chl="+ URLEncoder.encode(latex),true);
+        respondLatex(message, event);
     }
 }
