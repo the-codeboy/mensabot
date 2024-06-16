adb546c6a8da54d19006205ebd9846020164e592
the_codeboy <mail@the-codeboy.com>
2024-06-16 14:31:01 +0200

    Remove some unused functions

--- src/main/java/com/the_codeboy/mensabot/util/Util.java
+++ src/main/java/com/the_codeboy/mensabot/util/Util.java
@@ -2,25 +2,18 @@ package com.the_codeboy.mensabot.util;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.the_codeboy.mensabot.data.UserData;
 import com.the_codeboy.mensabot.data.UserDataManager;
-import com.the_codeboy.mensabot.events.CommandEvent;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import org.mariuszgromada.math.mxparser.Expression;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
@@ -29,37 +22,6 @@ public class Util {
     private static final Random rand = new Random();
     private static final Color[] colors = new Color[]{Color.blue, Color.cyan, Color.magenta, Color.orange, Color.pink, Color.yellow, Color.white};
     private static final String arrow = "↑";
-
-    public static String toReadable(long millis) {
-        int seconds = (int) (millis / 1000);
-        int years = seconds / secondsPerYear;
-        seconds -= years * secondsPerYear;
-        int months = seconds / secondsPerMonth;
-        seconds -= months * secondsPerMonth;
-        int days = seconds / secondsPerDay;
-        seconds -= days * secondsPerDay;
-        int hours = seconds / 3600;
-        seconds -= hours * 3600;
-        int minutes = seconds / 60;
-        seconds -= minutes * 60;
-
-        String date = "";
-        if (years > 0)
-            date += years + " year" + (years > 1 ? "s " : " ");
-        if (months > 0)
-            date += months + " month" + (months > 1 ? "s " : " ");
-        if (days > 0)
-            date += days + " day" + (days > 1 ? "s " : " ");
-        if (hours > 0)
-            date += hours + " hour" + (hours > 1 ? "s " : " ");
-        if (minutes > 0)
-            date += minutes + " minute" + (minutes > 1 ? "s " : " ");
-        if (seconds > 0)
-            date += seconds + " second" + (seconds > 1 ? "s " : " ");
-        if (date.length() == 0)
-            date = millis + " milliseconds";
-        return date;
-    }
 
     public static String repeat(String s, int times) {
         StringBuilder builder = new StringBuilder();
@@ -77,22 +39,8 @@ public class Util {
         return output;
     }
 
-    public static EmbedBuilder getSongInfo(AudioTrack track, EmbedBuilder builder) {
-        builder.setTitle(track.getInfo().title);
-        builder.setDescription(" by " + track.getInfo().author);
-        builder.addField("Duration", Util.toReadable(track.getPosition()) + "/" + Util.toReadable(track.getDuration()), true);
-        builder.addField("Time left: " + Util.toReadable(track.getDuration() - track.getPosition() - 1),  //subtract one millisecond extra because songs weirdly always end one millisecond early
-                Util.getProgress(20, (double) track.getPosition() / track.getDuration(), ":white_large_square:", ":black_large_square:"), false);
-        return builder;
-    }
-
     private static final int secondsPerDay = 3600 * 24,
             secondsPerMonth = secondsPerDay * 30, secondsPerYear = secondsPerDay * 365;
-
-
-    public static int toInt(String string) {
-        return toInt(string, 0);
-    }
 
     public static int toInt(String string, int defaultValue) {
         try {
@@ -102,31 +50,7 @@ public class Util {
         return defaultValue;
     }
 
-    public static boolean isInt(String s) {
-        try {
-            Integer.parseInt(s);
-            return true;
-        } catch (Exception ignored) {
-        }
-        return false;
-    }
-
-    public static EmbedBuilder sign(EmbedBuilder builder, CommandEvent e) {
-        builder.setFooter("requested by " + e.getMember().getEffectiveName(), e.getMember().getUser().getAvatarUrl());
-        return builder;
-    }
-
-    public static Member getAsMember(User user) {
-        if (user.getMutualGuilds().isEmpty())
-            return null;
-        return user.getMutualGuilds().get(0).getMember(user);
-    }
-
     public static Color getRandomColor() {
-//        float r = rand.nextFloat();
-//        float g = rand.nextFloat();
-//        float b = rand.nextFloat();
-//        return new Color(r,g,b);
         return colors[rand.nextInt(colors.length)];
     }
 
@@ -134,29 +58,6 @@ public class Util {
         UserData data = UserDataManager.getInstance().getData(user);
         data.setKarma(data.getKarma() + amount);
         UserDataManager.getInstance().save(data);
-    }
-
-    public static BufferedImage getAvatarImage(User user) throws IOException {
-        return getImageFromUrl(user.getEffectiveAvatarUrl());
-    }
-
-    public static BufferedImage getImageFromUrl(String adress) throws IOException {
-        URL url = new URL(adress);
-        URLConnection hc = url.openConnection();
-        hc.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
-        return ImageIO.read(hc.getInputStream());
-    }
-
-    public static BufferedImage ensureOpaque(BufferedImage bi) {
-        if (bi.getTransparency() == BufferedImage.OPAQUE)
-            return bi;
-        int w = bi.getWidth();
-        int h = bi.getHeight();
-        int[] pixels = new int[w * h];
-        bi.getRGB(0, 0, w, h, pixels, 0, w);
-        BufferedImage bi2 = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-        bi2.setRGB(0, 0, w, h, pixels, 0, w);
-        return bi2;
     }
 
     public static <T> void shuffle(T[] a) {
