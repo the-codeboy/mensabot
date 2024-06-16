3f9919283afc8a492b544b3201f2c9a2f73066a2
the_codeboy <mail@the-codeboy.com>
2024-06-16 14:02:17 +0200

    Allow help output to be split in multiple embeds

--- src/main/java/com/the_codeboy/mensabot/commands/Help.java
+++ src/main/java/com/the_codeboy/mensabot/commands/Help.java
@@ -3,8 +3,11 @@ package com.the_codeboy.mensabot.commands;
 import com.the_codeboy.mensabot.Bot;
 import com.the_codeboy.mensabot.events.CommandEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Help extends Command {
     private final Bot bot;
@@ -20,10 +23,18 @@ public class Help extends Command {
         Collection<Command> commands = bot.getCmdHandler().getCommands();
         EmbedBuilder embedBuilder = newBuilder();
         embedBuilder.setTitle("Help for commands");
+        List<MessageEmbed> embeds = new ArrayList<>();
         for (Command command : commands) {
             if (!command.isHidden())
                 embedBuilder.addField(command.getName(), command.getDescription(), true);
+            if (embedBuilder.getFields().size() >= 25) {
+                embeds.add(embedBuilder.build());
+                embedBuilder = newBuilder();
+                embedBuilder.setTitle("Help page " + (embeds.size() + 1));
+            }
         }
-        event.reply(embedBuilder);
+        if (!embedBuilder.getFields().isEmpty())
+            embeds.add(embedBuilder.build());
+        event.reply(embeds.toArray(new MessageEmbed[0]));
     }
 }
